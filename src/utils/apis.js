65437411acc4498800786cffdf3362705cbbadb6
jenson <jensonlong@aliyun.com>
2023-06-05 15:48:04 +0800

    fix:环境变量

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,8 +1,14 @@
 import Axios from 'axios'
 import { ElMessage } from 'element-plus'
 
+// 服务器链接
+const VITE_TARGET = import.meta.env.VITE_TARGET
+
 export const Get = async (url, data) => {
   return await new Promise((resolve, reject) => {
+    if (import.meta.env.MODE === 'production') {
+      url = `${VITE_TARGET}${url}`
+    }
     Axios.get(url, {
       params: data,
     }).then(res => {
@@ -19,6 +25,9 @@ export const Get = async (url, data) => {
 
 export const Post = async (url, data) => {
   return await new Promise((resolve, reject) => {
+    if (import.meta.env.MODE === 'production') {
+      url = `${VITE_TARGET}${url}`
+    }
     Axios.post(url, data, {
       headers: 'application/json',
     }).then(res => {
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
@@ -6,42 +6,46 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import ElementPlus from 'unplugin-element-plus/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    ElementPlus(), // elementUI 样式
-    // ElementPlus懒加载
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    // ElementPlus懒加载
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': '/src',
-    }
-  },
-  server: {
-    proxy: {
-      '/youdao': {
-        target: 'https://openapi.youdao.com/api',
-        changeOrigin: true,
-      },
-      '/category': {
-        target: 'http://127.0.0.1:3000',
-      },
-      '/card': {
-        target: 'http://127.0.0.1:3000',
-      },
-      '/case': {
-        target: 'http://127.0.0.1:3000',
-      },
-      '/type': {
-        target: 'http://127.0.0.1:3000',
-      },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) // 环境变量
+
+  return {
+    plugins: [
+      vue(),
+      ElementPlus(), // elementUI 样式
+      // ElementPlus懒加载
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      // ElementPlus懒加载
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': '/src',
+      }
+    },
+    server: {
+      proxy: {
+        '/youdao': {
+          target: 'https://openapi.youdao.com/api',
+          changeOrigin: true,
+        },
+        '/category': {
+          target: env.VITE_TARGET,
+        },
+        '/card': {
+          target: env.VITE_TARGET,
+        },
+        '/case': {
+          target: env.VITE_TARGET,
+        },
+        '/type': {
+          target: env.VITE_TARGET,
+        },
+      }
     }
   }
 })
